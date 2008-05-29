f1dca625fb479d3a0ab6b60be299f1bbcb726cf6
Ricardo SIGNES <rjbs@cpan.org>
2008-05-29 10:59:26 -0400

    git!

---
+++ lib/Config/INI/MVP/Reader.pm
@@ -0,0 +1,148 @@
+use strict;
+use warnings;
+
+package Config::INI::MVP::Reader;
+use base qw(Config::INI::Reader);
+
+=head1 NAME
+
+Config::INI::MVP::Reader - multi-value capable .ini file reader (for plugins)
+
+=head1 VERSION
+
+version 0.016
+
+=cut
+
+our $VERSION = '0.016';
+
+=head1 DESCRIPTION
+
+The MVP INI file reader reads INI files, but can handle properties with
+multiple values.  The identification of properties that may have multiple
+entries is done by section, on a plugin basis.  For example, given the
+following file:
+
+  [Foo::Bar]
+  x = 1
+  y = 2
+  y = 3
+
+MVP will, upon reaching this section, load Foo::Bar and call a method (by
+default C<multivalue_args>) on it, to determine which property names may have
+multiple entries.  If the return value of that method includes C<y>, then the
+entry for C<y> in the Foo::Bar section will be an arrayref with two values.  If
+the list returned by C<multivalue_args> did not contain C<y>, then an exception
+would be raised while reading this section.
+
+=cut
+
+sub new {
+  my ($class) = @_;
+
+  my $self = bless { } => $class;
+
+  $self->{__PACKAGE__} = {
+    mva    => { $self->starting_section => [ $self->multivalue_args ] },
+    order  => [ $self->starting_section ],
+    # plugin => { $self->starting_section => { } },
+  };
+
+  return $self;
+}
+
+sub multivalue_args { }
+
+sub starting_section { q{=} }
+
+sub change_section {
+  my ($self, $section) = @_;
+
+  my ($name, $package) = $section =~ m{\A\s*(?:([^/\s]+)\s*/\s*)?(\S+)\z};
+  $name = $package unless defined $name and length $name;
+  
+  Carp::croak qq{couldn't understand section header: "$section"}
+    unless $package;
+
+  # Consider using Params::Util to validate class name.  -- rjbs, 2007-05-11
+  Carp::croak "invalid package name '$package' in configuration"
+    unless $package =~ /\A[A-Z0-9]+(?:::[A-Z0-9]+)*\z/i;
+  
+  Carp::croak qq{multiple sections found for plugin "$name"}
+    if $self->{__PACKAGE__}{plugin}{$name};
+
+  my $plugin = $self->{__PACKAGE__}{plugin}{$name} = {
+    '=package' => $package
+  };
+
+  push @{ $self->{__PACKAGE__}{order} }, $name;
+  $self->{section} = $name;
+
+  # We already inspected this plugin.
+  return if $self->{__PACKAGE__}{mva}{$package};
+
+  eval "require $package; 1"
+    or Carp::croak "couldn't load plugin $section given in config: $@";
+
+  if ($section->can('multivalue_args')) {
+    $self->{__PACKAGE__}{mva}{$package} = [ $section->multivalue_args ];
+  } else {
+    $self->{__PACKAGE__}{mva}{$package} = [ ];
+  }
+}
+
+sub set_value {
+  my ($self, $name, $value) = @_;
+
+  my $sec_name = $self->current_section;
+  my $section = $self->{__PACKAGE__}{plugin}{ $sec_name } ||= {};
+
+  my $mva = $self->{__PACKAGE__}{mva}{ $sec_name };
+
+  if (grep { $_ eq $name } @$mva) {
+    $section->{$name} ||= [];
+    push @{ $section->{$name} }, $value;
+    return;
+  }
+
+  if (exists $section->{$name}) {
+    Carp::croak "multiple values given for property $name in section $sec_name";
+  }
+
+  $section->{$name} = $value;
+}
+
+sub finalize {
+  my ($self) = @_;
+
+  my $data = $self->{data} = [ ];
+  for my $name (@{ $self->{__PACKAGE__}{order} }) {
+    my $plugin = $self->{__PACKAGE__}{plugin}{$name};
+    $plugin->{'=name'} = $name;
+    push @$data, $plugin;
+  }
+}
+
+=head1 AUTHOR
+
+Ricardo SIGNES, C<< <rjbs@cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org>.  I will be notified, and then you'll automatically be
+notified of progress on your bug as I make changes.
+
+=head1 COPYRIGHT
+
+Copyright 2008 Ricardo SIGNES, all rights reserved.
+
+This program is free software; you may redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
+
+1;
+
